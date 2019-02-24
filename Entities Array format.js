5e380e0618054c8951ea37ca3a85e1b18b04067b
isanad <47346767+isanad@users.noreply.github.com>
2019-02-24 10:15:41 +0200

    Update Entities Array format.js
    proofread

--- Entities Array format.js
+++ Entities Array format.js
@@ -1,25 +1,31 @@
 
-const User =[{name: "", id: "", username:"", password: "", e.mail:"", dateofregistration: "", phonenumber:"", usertype:"",
-location: "", },];
+const User =[{name: "", id: "", username:"", password: "", email:"", dateofregistration: "", phonenumber:"", usertype:"",
+location: ""},];
 
-const Partners =[{organisationname: "", basicinformation:"", partners:"", boardmembers:"", eventsorganized:"", fieldofwork:"", projecthistory:"", feedbackform:"", },]; (inherited from user)
+//inherited from user
+const Partners =[{organisationname: "", basicinformation:"", partners:"", boardmembers:"", eventsorganized:"", fieldofwork:"",
+                  projecthistory:"", feedbackform:"" },]; 
 
-const Admins =[{},]; (inherited from user)
+//inherited from user
+const Admins =[{},]; 
 
-const Candidates =[{age:"", skills:"", interests:"", pastevents/tasks:"", reviewsrecieved:"", certificates/masterclasses:"", },]; (inherited from user)
+//inherited from user
+const Candidates =[{age:"", skills:"", interests:"", pasteventstasks:"", reviewsrecieved:"", certificatesandmasterclasses:""},];
 
 const Events =[{organizer:"", location:"", description:"",
- registrationprice:"", remainingplaces:"", speakers:"", maximumplaces:"", topicscovered:"", feedbackid:"", field:"", approvalstatus:"", },];
+ registrationprice:"", remainingplaces:"", speakers:"", maximumplaces:"", topicscovered:"",
+                feedbackid:"", field:"", approvalstatus:""},];
 
-const Tasks =[{Description:"", eta:"", levelofcommitment:"", partner:"", monetarycompensation:"", skills:"", consutancy:"", lifecyclestatus:"", experienceneeded:"",},];
+const Tasks =[{Description:"", eta:"", levelofcommitment:"", partner:"", monetarycompensation:"", skills:"", consutancy:"",
+               lifecyclestatus:"", experienceneeded:""},];
 
-const Notifications =[{sender:"", reciever:"", time:"", type:"", message:"",},];
+const Notifications =[{sender:"", reciever:"", time:"", type:"", message:""},];
 
-const Bookings =[{event:"", partner:"", attendee:"", dateofbooking:"", bookingnumber:"",},];
+const Bookings =[{event:"", partner:"", attendee:"", dateofbooking:"", bookingnumber:""},];
 
-const Applications =[{candidate:"", partner:"", datetime:"", task:"", status:"",},];
+const Applications =[{candidate:"", partner:"", datetime:"", task:"", status:""},];
 
-const Feedbacks =[{id:"", response:"", name:"",},];
+const Feedbacks =[{id:"", response:"", name:""},];
 
 
 
