5f7fc1695d9bd68d0fec01bd434d4edbe31f1e09
isanad <47346767+isanad@users.noreply.github.com>
2019-02-26 09:44:13 +0200

    adjusted variable names.

---
+++ Application.js
@@ -0,0 +1,15 @@
+//const Application=required('Application');
+//const Aid =required('Aid');
+
+class Applications{
+    constructor(candidate,partner,datetime,task,status){
+        this.candidate=candidate;
+        this.partner=partner;
+        this.datetime=datetime;
+        this.task=task;
+        this.status=status;
+        this.id=Aid.v4();   
+    }
+}
+
+module.exports(Applications)
