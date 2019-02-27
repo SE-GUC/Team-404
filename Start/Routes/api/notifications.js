d1d506cb05bbf312871ba2af34ef582c7e4e4199
IsmailAboulela <ismail.aboulela@gmail.com>
2019-02-27 21:51:35 +0200

    Update notifications.js

--- Start/Routes/api/notifications.js
+++ Start/Routes/api/notifications.js
@@ -1,11 +1,44 @@
 
 // temporary data created as if it was pulled out of the database ...
+//const uuid = require('uuid')
+const express = require("express");
+// Create the app
+const app = express();
+// Use it with post
+app.use(express.json());
+
+const router = express.Router()
+const Notification = require('../../models/Notification')
+
+
 const notifications = [
-    new Notification('5', 'Ismail Aboulela', 'Hagar Abdelnabi', '1:00', 'accept/reject', 'hello'),
-    new Notification('6', 'Gomana El-Shimy', 'Hagar Abdelnabi', '1:01', 'normal', 'hi'),
-    new Notification('8', 'Islam Sanad', 'Karim El-Mahdy', '1:02', 'normal', 'hey'),
-    new Notification('9', 'Ismail Aboulela', 'Lobna El-Badrawy', '1:03', 'accept/reject', 'hola')
+    new Notification('notification text 1'),
+    new Notification('notification text 2'),
+    new Notification('notification text 3'),
+    new Notification('notificatoin text 4')
+    
 ];
+
+router.post('/', (req, res) => {
+	const text = req.body.text;
+
+	if (!text) return res.status(400).send({ err: 'notification text required' });
+	if (typeof text !== 'string') return res.status(400).send({ err: 'Invalid value for text' });
+	
+
+	const newNotification = {
+		Notification(text)
+		
+	};
+
+	notifications.push(newNotification)
+	return res.json({ data: newNotification });
+	
+	
+	
+});
+
+
 
 router.get('/', (req, res) => res.json({ data: notifications }))
 
