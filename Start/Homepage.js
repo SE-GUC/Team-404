28174be0a555651e65057fe9f5f28a6c5e9fd953
LobnaElBadrawy <lobna.elbadrawy@gmail.com>
2019-03-01 21:32:20 +0200

    Tasks update is now operating.
    Update and read is now working

--- Start/Homepage.js
+++ Start/Homepage.js
@@ -1,15 +1,25 @@
 // Import express
-const express = require('express')
+const express = require("express")
+const uuid = require('uuid')
 // Create the app
 const app = express()
 // Use it with post
 app.use(express.json())
 
 //shows a message on the homepage indicated by '/' directory
-app.get('/' ,(req,res) => {
-  res.send('<h1>Welcome Team404</h1>')
+const tasks = require ("./Routes/api/tasks")
+
+app.get("/" ,(req,res) => {
+  res.send(`<h1>Welcome Team404</h1> <a href = "api/tasks"> tasks </a>`)
+  
 }
 )
+
+app.use("/api/tasks",tasks);
+
+app.use ((req,res)=>{
+  res.status(404).send({err:"we cannot find what you're looking for"})
+})
 
 //opens the port at 3000
 const port = process.env.PORT | 3000
--- Start/Routes/api/tasks.js
+++ Start/Routes/api/tasks.js
@@ -1,20 +1,103 @@
 //routings for task
 
 const express = require('express')
+const app = express()
+app.use(express.json())
 const router = express.Router()
+const uuid = require('uuid')
+const Task = require ('../../Models/Task')
 
 // We will be connecting using database 
 //const Book = require('../../models/Book')
 
 // temporary data created as if it was pulled out of the database ...
 const tasks = [
-    new Task(1, 'help me', '140 days', 'low', 'isanad', 400, 'Microsoft', 'pending', 0),
-    new Task(2, 'kill me', '1 month', 'high', 'gomanaelshimy', 5000, 'hitman', 'done', 5),
-    new Task(3, 'feed me', '1 month', 'low', 'gomanaelshimy', 3000, 'Chef', 'in progress', 3),
-    new Task(4, 'drive me', 'high', 'ismailaboulela', 10000, 'Driving', 'in progress', 2),
+    new Task("aaaaaaa", 1, "high", "lob", 2000, "skilllz", "early", "0 years","advisor 1" ),
+    new Task("bbbbbbb", 2, "medium", "aasdf", 2000, "that", "early", "1 year", "advisor 2" ),
+    new Task("ccccccc", 3, "low", "papapa", 5000, "pay", "early", "2 years", "adivsor 3" ),
+    new Task("ddddddd", 4, "high", "zzzzz", 3000, "the bills", "early", "3 years", "advisor 4" ),
+
 ];
 
 router.get('/', (req, res) => res.json({ data: tasks }))
 
+// update task
+router.put('/:id', (req, res) => {
+    const tasksID = req.params.id 
+    const Task = tasks.find(Task => Task.id === tasksID)
+
+    const Description            = req.body.Description
+    const ETA                    = req.body.eta
+    const Levelofcommitment      = req.body.levelofcommitment
+    const Partner                = req.body.partner
+    const Monetarycompensation   = req.body.monetarycompensation
+    const Skills                 = req.body.skills
+    const Lifecyclestatus        = req.body.lifecyclestatus
+    const Experienceneeded       = req.body.experienceneeded
+    const Consultancy            = req.body.consultancy
+
+
+
+    if(Description){
+       // const Description = req.body.Description 
+        Task.Description = Description 
+    }
+    if(ETA){
+        //const ETA = req.body.eta 
+        Task.eta = ETA 
+    }
+    if(Levelofcommitment){
+        //const Levelofcommitment = req.body.levelofcommitment 
+        Task.levelofcommitment = Levelofcommitment 
+    }
+    if(Partner){
+      //  const Partner = req.body.partner 
+        Task.partner = Partner 
+    }
+    if(Monetarycompensation){
+        //const Monetarycompensation = req.body.monetarycompensation 
+        Task.monetarycompensation = Monetarycompensation 
+    }
+    if(Skills){
+       // const Skills = req.body.skills 
+        Task.skills = Skills 
+    }
+    if(Lifecyclestatus){
+       // const Lifecyclestatus = req.body.lifecyclestatus 
+        Task.lifecyclestatus = Lifecyclestatus 
+    }
+    if(Experienceneeded){
+       // const Experienceneeded = req.body.experienceneeded 
+        Task.experienceneeded = Experienceneeded 
+    }
+    if(Consultancy){
+       // const Consultancy = req.body.consultancy 
+        Task.consultancy = Consultancy 
+    }
+
+    //Task.Description             = Description
+    //Task.eta                     = ETA
+    //Task.levelofcommitment       = Levelofcommitment
+    //Task.partner                 = Partner
+    //Task.monetarycompensation    = Monetarycompensation
+    //Task.skills                  = Skills
+    //Task.consultancy             = Consultancy
+    //Task.lifecyclestatus         = Lifecyclestatus
+    //Task.experienceneeded        = Experienceneeded
+    
+    //tasks.push(tasks)
+    res.send(tasks)
+})
+
+
+// Delete a task
+//app.delete('/api/tasks/:ID', (req, res) => {
+  //  const tasksID = req.params.id 
+
+    //const tasks = tasks.find(tasks => tasks.id === tasksID)
+    //const index = tasks.indexOf(tasks)
+    //tasks.splice(index,1)*9
+    //res.send(tasks)
+//})
 module.exports = router
 
