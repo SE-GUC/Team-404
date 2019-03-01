c3e1b7eb8eb0aa8c5addc07d17b8e42589b9defe
IsmailAboulela <ismail.aboulela@gmail.com>
2019-03-01 21:25:06 +0200

    merging optimization
    merging optimization

--- Start/Models/Booking.js
+++ Start/Models/Booking.js
@@ -3,12 +3,17 @@
 const uuidv1 = require('uuid/v1')
 
 class Booking{
-    constructor(event,partner,attendee,dateofbooking){
+    constructor(event,partner,attendee){
+        var today       = new Date();
+        var date        = today.getFullYear() + '';
+        var time        = today.getHours() + '';    
+        var dateTime    = date + time;
     this.event=event;
     this.partner=partner;
     this.attendee=attendee;
     this.dateofbooking=dateofbooking;
     this.id=uuidv1();
+    this.dateofbooking = dateTime
     };
 
 };
--- Start/Routes/api/bookings.js
+++ Start/Routes/api/bookings.js
@@ -10,16 +10,16 @@ const Booking = require('../../models/Booking')
 
 // temporary data created as if it was pulled out of the database ...
 const bookings = [
-    new Booking('photoshop for everyone', 'Islam Sanad', 'Lobna El-Badrawy',4/3/2018 ),
-    new Booking('photoshop for everyone', 'Hania Ghannam', 'Omar Yasser', 4/3/2018 ),
-    new Booking('design fundementals', 'Clara Kamel', 'Karim El-Mahdy',18/5/2018),
-    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser', 4/3/2018),
-    new Booking('design fundementals', 'Clara Kamel', 'Karim El-Mahdy', 18/5/2018),
-    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser', 4/3/2018),
-    new Booking('design fundementals', 'Clara Kamel', 'Karim El-Mahdy', 18/5/2018),
-    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser', 4/3/2018),
-    new Booking('be a programmer', 'Clara Kamel', 'Karim El-Mahdy', 18/5/2018),
-    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser',4/3/2018 ),
+    new Booking('photoshop for everyone', 'Islam Sanad', 'Lobna El-Badrawy'),
+    new Booking('photoshop for everyone', 'Hania Ghannam', 'Omar Yasser'),
+    new Booking('design fundementals', 'Clara Kamel', 'Karim El-Mahdy'),
+    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser'),
+    new Booking('design fundementals', 'Clara Kamel', 'Karim El-Mahdy'),
+    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser'),
+    new Booking('design fundementals', 'Clara Kamel', 'Karim El-Mahdy'),
+    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser'),
+    new Booking('be a programmer', 'Clara Kamel', 'Karim El-Mahdy'),
+    new Booking('be a programmer', 'Hania Ghannam', 'Omar Yasser'),
 
 
 ];
