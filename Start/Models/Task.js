49faef70d74f17756a7d7450d8a6d2095d016f22
Youssef Nagy <youssefnkamal@gmail.com>
2019-03-21 15:48:38 +0200

    Add files via upload

--- Start/Models/Task.js
+++ Start/Models/Task.js
@@ -1,18 +1,17 @@
-const uuid = require('uuid') 
+const uuid = require('uuid')
 class Task {
-    constructor(Description, eta, levelofcommitment, partner, monetarycompensation, skills, lifecyclestatus, experienceneeded,consultancy ) {
-        this.id                    = uuid.v4();
-        this.Description            = Description;
-        this.eta                    = eta;
-        this.levelofcommitment      = levelofcommitment;
-        this.partner                = partner;
-        this.monetarycompensation   =  monetarycompensation;
-        this.skills                 = skills;
-        this.lifecyclestatus        = lifecyclestatus;
-        this.experienceneeded       = experienceneeded;
-        this.consultancy            = consultancy;
-        
-    };
+  constructor (Description, eta, levelofcommitment, partner, monetarycompensation, skills, lifecyclestatus, experienceneeded, consultancy) {
+    this.id = uuid.v4()
+    this.Description = Description
+    this.eta = eta
+    this.levelofcommitment = levelofcommitment
+    this.partner = partner
+    this.monetarycompensation = monetarycompensation
+    this.skills = skills
+    this.lifecyclestatus = lifecyclestatus
+    this.experienceneeded = experienceneeded
+    this.consultancy = consultancy
+  };
 }
 
 module.exports = Task
