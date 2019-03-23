303efa80de91736ad485b244b44ee0e9797145c2
IsmailAboulela <47501009+IsmailAboulela@users.noreply.github.com>
2019-03-23 17:58:09 +0200

    Add files via upload

--- Start/Models/Notification.js
+++ Start/Models/Notification.js
@@ -1,20 +1,17 @@
-const uuid = require('uuid/v4')
 
-class Notification {
-  constructor (text) {
-    var today = new Date()
-    var date = today.getFullYear() + ''
-    var time = today.getHours() + ''
-    var dateTime = date + time
-    this.id = uuid()
-    this.text = text
-    this.time = dateTime
-  };
-};
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+const ObjectId = schema.Types.ObjectId
+const NotificationSchema = new schema({
+  _id: ObjectId,
+  text: { type: String, required: false, unique: false },
+  date: { type: Date, default: Date.now() },
+  recipient: { type: String, required: true, unique: false },
+  trigger: { type: String, required: true, unique: false }
+})
+
+// NotificationSchema.index({ candidate: 1, partner: 1, task: 1 }, { unique: true })
+
+const Notification = mongoose.model('Notification', NotificationSchema)
+
 module.exports = Notification
-// function SendNotif(text, notified, Notification) {
-//    Notification(text,time)
-//    array.forEach(notified => {
-//        notified.notifications.push(Notification)
-//    });
-// }
