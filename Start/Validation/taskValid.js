64293f97b4615c0d7b1b7f0b5d045636f5588134
IsmailAboulela <47501009+IsmailAboulela@users.noreply.github.com>
2019-03-23 17:01:53 +0200

    Update taskValid.js

--- Start/Validation/taskValid.js
+++ Start/Validation/taskValid.js
@@ -1,22 +1,6 @@
 const Joi = require('joi')
 
 module.exports = {
-  createValidation: request => {
-    const createSchema = {
-      Description: Joi.string().required(),
-      eta: Joi.number().required(),
-      levelofcommitment: Joi.string().required(),
-      partner: Joi.string().required(),
-      monetarycompensation: Joi.number().required(),
-      skills: Joi.string().required(),
-      lifecyclestatus: Joi.string().required(),
-      experienceneeded: Joi.string().required(),
-      consultancy: Joi.string().required()
-    }
-
-    return Joi.validate(request, createSchema)
-  },
-
   updateValidation: request => {
     const updateSchema = {
       Description: Joi.string(),
