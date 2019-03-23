417d123056222b574d705a03d4b4aba3d4ef3829
IsmailAboulela <47501009+IsmailAboulela@users.noreply.github.com>
2019-03-23 17:00:50 +0200

    Update tasks.js

--- Start/Routes/api/tasks.js
+++ Start/Routes/api/tasks.js
@@ -11,27 +11,6 @@ router.get('/', async (req, res) => {
   const tasks = await Task.find()
   res.json({ data: tasks })
 })
-router.post('/', async (req, res) => {
-  try {
-    const isValidated = validator.createValidation(req.body)
-    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-    const task = await new Task({
-      _id: mongoose.Types.ObjectId(),
-      Description: req.body.Description,
-      eta: req.body.eta,
-      levelofcommitment: req.body.levelofcommitment,
-      partner: req.body.partner,
-      monetarycompensation: req.body.monetarycompensation,
-      skills: req.body.skills,
-      lifecyclestatus: req.body.lifecyclestatus,
-      experienceneeded: req.body.experienceneeded,
-      consultancy: req.body.consultancy
-    }).save()
-    return res.json({ data: task })
-  } catch (error) {
-    console.log(error)
-  }
-})
 
 router
   .route('/:id')
@@ -43,14 +22,6 @@ router
       return response.json({ error: status.error.details[0].message })
     }
     next()
-  })
-  .get(async (request, response) => {
-    try {
-      const task = await Task.findById(request.params.id).exec()
-      return response.json({ data: task })
-    } catch (err) {
-      return response.json({ error: `Error, couldn't find a task given the following id` })
-    }
   })
   .put(async (request, response) => {
     Task.findByIdAndUpdate(request.params.id, request.body, { new: true }, (err, model) => {
