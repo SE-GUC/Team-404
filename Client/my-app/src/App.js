8869673b9c59604630a8ca6ce42a91a003d1dc5b
isanad <islam.sanad@student.guc.edu.eg>
2019-04-06 09:31:18 +0200

    ToDo List works , gonna start application

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Todo from "./Components/ToDo";
+import Header from './Components/Layout/Header';
+import AddTodo  from "./Components/AddTodo";
+ 
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "do it ",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "be a man",
+        completed: false
+      }
+    ]
+  };
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  delTodo = (id) => {
+    this.setState({todos:[...this.state.todos.filter(todo => todo.id !==id)]})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           Hello Team 404 :D
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Header/>
+      <AddTodo/>
+        <Todo todo={this.state.todos} delTodo = {this.delTodo} markComplete={this.markComplete} />
       </div>
     );
   }
