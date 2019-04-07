d64d06003da9f982c953c07948b57092242b76c8
isanad <islam.sanad@student.guc.edu.eg>
2019-04-07 20:45:33 +0200

    switched to the new file system

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Team-404
 
-Hello , I seperated the code into two folders Client and Server. Server is for the backend and Client for the front end.
-All you need to do in the Client folder is just do npm i to download the node_modules folder and then everything should work perfectly.
-Check the sprint4 backlog to know your tasks , goodluck :D
+Team 404 Lirten Hub
+
+Hey there:D, i created this repository to start off the project. No one else can add or edit except the team members. We'll use this to add any sort of revision points or maybe to update our position all of us too see and keep track and stuff. Any comments that you wanna add or advice or anything can be added here. Goodluck dudes.
+
+
+Here's a list of git commands that are commonly used : https://github.com/joshnh/Git-Commands
+Heroku app: https://team404sprint2.herokuapp.com/
---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "./App.css";
+import Application from "./Components/Application";
+import Header from "./Components/Layout/Header";
+import ApplicationInfo from "./Components/ApplicationInfo";
+import Axios from "axios";
+
+class App extends Component {
+  state = {
+    application: []
+  };
+
+  /* application information
+  attempt to put each information in an array and display all of em
+  supposed to display the skills required only in task not from candidate
+*/
+
+  componentDidMount() {
+    Axios.get("http://localhost:3001/api/applications").then(res =>
+      console.log(res.data)
+    );
+    // .then(res => this.setState({ application: res.application }));
+  }
+
+  delApplication = id => {
+    this.setState({
+      application: [
+        ...this.state.application.filter(application => application.id !== id)
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div style={together}>
+          <Header />
+          <ApplicationInfo
+            applicationInfo={this.state.application}
+            delApplication={this.delApplication}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const together = {
+  border: "10px",
+  borderRadius: "10px",
+  background: "#9D9FB9",
+  margin: "auto",
+  paddingBottom: "1px"
+};
+
+export default App;
