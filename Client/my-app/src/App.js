29890fae11b2df9ec4009bf3bac28324fd6c3c43
KarimElMahdy <47499362+KarimElMahdy@users.noreply.github.com>
2019-04-07 23:02:24 +0200

    REACT

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import PartnerHeader from './components/PartnerHeader';
+//import Search from './components/Search';
+//import Partner from './components/Partner';
+//import Table from './components/Table';
+import PartnerPage from './components/PartnerPage';
+
 
 class App extends Component {
+  state = {
+    partner : [
+      {
+      id : 1,
+      organisationname: 'p1-OrganisationName',
+      partners:'p1-Partners',
+      boardmembers: 'p1-BoardMembers',
+      eventsorganized:'p1-EventsOrganized',
+      fieldofwork: 'p1-FieldOfWork',
+      projecthistory:'p1-ProjectHistory',
+      feedbackform: 'p1-FeedBackForm'
+      },
+      {
+        id : 2,
+        organisationname: 'p2-OrganisationName',
+        partners:'p2-Partners',
+        boardmembers: 'p2-BoardMembers',
+        eventsorganized:'p2-EventsOrganized',
+        fieldofwork: 'p2-FieldOfWork',
+        projecthistory:'p2-ProjectHistory',
+        feedbackform: 'p2-FeedBackForm'
+        },
+        {
+          id : 3,
+          organisationname: 'p3-OrganisationName',
+          partners:'p3-Partners',
+          boardmembers: 'p3-BoardMembers',
+          eventsorganized:'p3-EventsOrganized',
+          fieldofwork: 'p3-FieldOfWork',
+          projecthistory:'p-3ProjectHistory',
+          feedbackform: 'p-3FeedBackForm'
+          }
+    ]
+  }
+ 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           Hello Team 404 :D
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App" /*style = {this.getStyle()}*/>
+          <div className="container">
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <PartnerHeader/>
+                <p style ={textStyle}>Welcome To Lirten Hub</p>
+              </React.Fragment>
+            )}/>
+            <Route path="/page" component={PartnerPage}/>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
+const textStyle ={
+  textAlign: 'left',
+  padding: '10px',
+  fontSize: '5mm'
 
+}
+
+console.log('This is a simple display of viewing the Partner entity by using dummy data, as well as a basic homepage.')
 export default App;
