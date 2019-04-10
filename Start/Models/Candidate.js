3dc31b1f5ad1ef1b86908ec89ee85f73f8d58745
OmarrNaserr <omar.ahmedsayed@student.guc.edu.eg>
2019-04-10 19:22:36 +0200

    React of user ready

---
+++ Start/Models/Candidate.js
@@ -0,0 +1,15 @@
+
+const mongoose=require('mongoose');
+const schema=mongoose.Schema;
+
+const CandidateSchema = new schema({
+    age:{type:Number,required:true,unique:false},
+    skills:{type:String,required:true,unique:false},
+    interests:{type:String,required:true,unique:false},
+    pasteventsandtasks:{type:String,required:true,unique:false},
+    reviewsrecieved:{type:String,required:true,unique:false},
+});
+
+const Candidate = mongoose.model('Candidate', CandidateSchema);
+
+module.exports = Candidate;
