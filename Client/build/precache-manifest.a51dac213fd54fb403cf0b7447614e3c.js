049896eb71014f054c0ef79d202029696a3aa1b5
isanad <islam.sanad@student.guc.edu.eg>
2019-04-11 22:24:14 +0200

    adding gitignore and trying to go to react_dev

---
+++ Client/build/precache-manifest.a51dac213fd54fb403cf0b7447614e3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e88cd8785074eff25449",
+    "url": "/static/js/main.931334b4.chunk.js"
+  },
+  {
+    "revision": "245abf76a3dff17921fc",
+    "url": "/static/js/2.de8ba4d2.chunk.js"
+  },
+  {
+    "revision": "e88cd8785074eff25449",
+    "url": "/static/css/main.b8956729.chunk.css"
+  },
+  {
+    "revision": "cb225c178b2ea0959246445816bf09c2",
+    "url": "/index.html"
+  }
+];
