6166b4d8f10d2c035cf6155d2002a8af806c0072
isanad <islam.sanad@student.guc.edu.eg>
2019-04-13 00:18:10 +0200

    added files

---
+++ client/src/components/page/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "../Login"
+function Login() {
+  return (
+    <React.Fragment>
+      <h1>Login</h1>
+    </React.Fragment>
+  );
+}
+
+export default Login;
---
+++ client/src/components/page/Tasks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Taskform() {
+  return (
+    <React.Fragment>
+      <h1>Taskform</h1>
+    </React.Fragment>
+  );
+}
+
+export default Taskform;
