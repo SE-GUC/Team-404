495defee8319c38bc48f8573d4ac2444b795a993
isanad <islam.sanad@student.guc.edu.eg>
2019-04-12 10:55:39 +0200

    Adjusted Task Model

--- Start/Models/Task.js
+++ Start/Models/Task.js
@@ -1,19 +1,21 @@
 
 const mongoose = require('mongoose')
+const candidate = require("./Candidate")
 const schema = mongoose.Schema
 const ObjectId = schema.Types.ObjectId
 const TaskSchema = new schema({
   _id: ObjectId,
-  Description: { type: String, required: true },
+  description: { type: String, required: true },
   eta: { type: Number, required: true },
-  levelofcommitment: { type: String, required: true, unique: false },
+  levelOfCommitment: { type: String, required: true, unique: false },
   partner: { type: String, required: true },
-  monetarycompensation: { type: Number, required: true },
-  skills: { type: String, required: true },
-  lifecyclestatus: { type: String, required: true },
-  experienceneeded: { type: String, required: true },
-  consultancy: { type: String, required: true }
-
+  monetaryCompensation: { type: Number, required: true },
+  skills: { type: String, enum: ["CS" , "Design" , "Word" , "Civil" , "Accounting"], required: true , lowercase: true },
+  lifeCycleStatus: { type: String, enum: ["Awaiting Approval" , "Denied" , "Pending Acceptance" , "Accepted" , "Completed" , "In Progress" ], required: true },
+  experienceNeeded: { type: String, required: true },
+  consultancyRequested: { type: Boolean, required: true },
+  consultant:{type: String , required: false},
+  applications: { type: [candidate], required: false }
 })
 
 // TaskSchema.index({ Description: 1, eta: 1, levelofcommitment: 1 }, { unique: true })
