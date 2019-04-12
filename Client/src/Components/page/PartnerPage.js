fa7639db01c008ce6c4bb1d1960a5820646077a5
RawiSherif <rawi.farrag@gmail.com>
2019-04-12 16:59:59 +0200

    Bugfixing

--- Client/src/Components/page/PartnerPage.js
+++ Client/src/Components/page/PartnerPage.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react'
+import React from 'react'
 import PartnerHeader from '../PartnerHeader';
 import PartnerTable from '../PartnerTable';
 function PartnerPage() {
