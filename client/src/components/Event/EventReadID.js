08256632dc798ef54173882f3d3dec1a50d2bff7
clarakamel <clara_atef@hotmail.com>
2019-04-13 21:36:48 +0200

    Edited EventReadID to make it look cleaner

--- client/src/components/Event/EventReadID.js
+++ client/src/components/Event/EventReadID.js
@@ -36,35 +36,45 @@ class EventReadID extends Component {
           <div className="card-content">
             <br />
             <br />
+            Name:
+            
+            <span className="event name">{event.eventName}</span>
             <br />
-            <span className="event candidate">{event.eventName}</span>
+            Organizer: {'  '}
+            
+            <span className="event organizer">{event.organizer}</span>
             <br />
+            Location: {'  '}
+            
+            <span className="event location">{event.location}</span>
             <br />
-            <span className="event candidate">{event.organizer}</span>
+            Description: {'  '}
+            
+            <span className="event description">{event.description}</span>
             <br />
+            Remaining Places: {'  '}
+            
+            <span className="event remaining places">{event.remainingPlaces}</span>
             <br />
-            <span className="event candidate">{event.location}</span>
+            Speakers: {'  '}
+            
+            <span className="event speakers">{event.speakers}</span>
             <br />
+            Maximum Places: {'  '}
+            
+            <span className="event maximum places">{event.maximumPlaces}</span>
             <br />
-            <span className="event candidate">{event.description}</span>
+            Topics Covered: {'  '}
+            
+            <span className="event's topics covered">{event.topicsCovered}</span>
             <br />
+            Field: {'  '}
+            
+            <span className="event field">{event.field}</span>
             <br />
-            <span className="event candidate">{event.remainingPlaces}</span>
-            <br />
-            <br />
-            <span className="event candidate">{event.speakers}</span>
-            <br />
-            <br />
-            <span className="event candidate">{event.maximumPlaces}</span>
-            <br />
-            <br />
-            <span className="event candidate">{event.topicsCovered}</span>
-            <br />
-            <br />
-            <span className="event candidate">{event.field}</span>
-            <br />
-            <br />
-            <span className="event candidate">{event.registrationPrice}</span>
+            Registration Price: {'  '}
+            
+            <span className="event registration price">{event.registrationPrice}</span>
             <br />
             <br />
             <Link to="/Event/update">UPDATE</Link>
