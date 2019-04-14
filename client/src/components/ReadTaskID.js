8e3f9ba94482111da671254f7266e74b75dc9d2c
isanad <islam.sanad@student.guc.edu.eg>
2019-04-14 21:53:23 +0200

    FIXIG

---
+++ client/src/components/ReadTaskID.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+class ReadTaskID extends Component {
+  state = {
+    tasks: []
+  };
+  componentDidMount() {
+    axios.get(`tasks/${this.props.match.params.id}`).then(res => {
+      console.log("Response is: ", res);
+      this.setState({ task: res.data.data });
+      console.log("The state is", this.state);
+    });
+  }
+
+  render() {
+
+    const { task } = this.state;
+    console.log("Task Log Is: ", task);
+    const taskJSX =
+      task && task._id ? (
+        <div className="task card" key={task._id}>
+          <div className="card-content">
+            <br />
+            <br />
+            <br />
+            <span className="task candidate">{task.description}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.eta}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.levelOfCommitment}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.partner}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.monetaryCompensation}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.skills}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.lifeCycleStatus}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.experienceNeeded}</span>
+            <br />
+            <br />
+            <span className="task candidate">{task.consultancyRequested}</span>
+            <br />
+            <br />
+        
+            {/* <Link to="/Event/update">UPDATE</Link>
+            <br />
+            <br /> */}
+            {/* <button className="btn btn-primay" onClick={this.onDelete}>Delete</button>
+            <br />
+            <br /> */}
+          </div>
+        </div>
+      ) : (
+        <div>Task you searched for is loading</div>
+      );
+    return taskJSX;
+  }
+} 
+export default withRouter(ReadTaskID);
---
+++ client/src/components/User/Login.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import Textbox from './Textbox';
+import "./Textbox.css";
+class Login extends Component {    
+  render() {
+    return (
+      <div id="background2">
+       <h1>Login page</h1>
+
+       <Textbox/>
+      </div>
+    );
+  }
+}
+export default Login;
