7054e0b96566f9dd6601481a695f3dd52bed5b9d
isanad <islam.sanad@student.guc.edu.eg>
2019-04-14 22:52:04 +0200

    maybe..

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
+import Header from "./Layout/Header";
 import PropTypes from "prop-types";
+import ApplicationInfo from "./ApplicationInfo";
 import "./Application.css";
 export class Application extends Component {
   render() {
+    const { id } = this.props.applicationInfo;
+
     return (
       //outputs the names , references
 
-      <div >
-        <p />
+      <div style={formFormat}>
+        <p>
+          <button
+            onClick={this.props.delApplication.bind(this, id)}
+            style={btnStyle}
+          >
+            X
+          </button>
+        </p>
       </div>
     );
   }
 }
 
+const btnStyle = {
+  background: "#f00",
+  color: "#fff",
+  padding: "10px",
+  borderRadius: "50%",
+  float: "right",
+  marginBottom: "40px"
+};
+
+const formFormat = {
+  background: "#4F5AF6",
+  padding: "5px 10px",
+  borderRadius: "5px",
+  borderBottom: "1px",
+  margin: "5px",
+  marginBottom: "20px"
+};
 
 //stays the type of the bigger class
 Application.propTypes = {
