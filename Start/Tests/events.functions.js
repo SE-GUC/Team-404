dc4e27342b8a8cc15b78d30fb1eb8f334b50d00a
clarakamel <clara_atef@hotmail.com>
2019-04-17 17:14:43 +0200

    The Tests for GET/POST/UPDATE for event are now working

--- Start/Tests/events.functions.js
+++ Start/Tests/events.functions.js
@@ -1,103 +1,40 @@
 const axios = require('axios');
-const Admin = require('../../Models/Admin')
-//const Request = require('../../Models/Request')
-const Event = require('../../Models/Event')
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 const functions = {
-
-//Partner request event    
-RequestEvent: async (req,res) => {
-  try {
-      
-    var Pid = req.params.id
-    
-    console.log('entry success')
-    
-    var newEvent = new Event({
-      
-      eventName: req.body.eventName,
-      organizer: Pid,
-      location: req.body.location,
-      remainingPlaces:req.body.remainingPlaces,
-      speakers:req.body.speakers,
-      maximumPlaces:req.body.maximumPlaces,
-      topicsCovered:req.body.topicsCovered,
-      field:req.body.field,
-      registrationPrice:req.body.registrationPrices,
-      approvalstaus:'pending',
-      applicants:req.body.applicants, 
-      feedback:req.body.feedback
-    });
-    console.log(newEvent)
-  }
-  catch(error) {
-  console.log('Could not create event')
-  }
-  },
-
-   
-//Admin create event 
-CreateEvent: async (req,res) => {
-    try {
-      
-      var Aid = req.params.id
-      
-      console.log('entry success')
-      
-      var newEvent = new Event({
-        
-        eventName: req.body.eventName,
-        organizer: Aid,
-        location: req.body.location,
-        remainingPlaces:req.body.remainingPlaces,
-        speakers:req.body.speakers,
-        maximumPlaces:req.body.maximumPlaces,
-        topicsCovered:req.body.topicsCovered,
-        field:req.body.field,
-        registrationPrice:req.body.registrationPrices,
-        approvalstaus:'confirmed',
-        applicants:req.body.applicants, 
-        feedback:req.body.feedback
-      });
-      console.log(newEvent)
-    }
-    catch(error) {
-    console.log('Could not create event')
-    }
-},
-
-// Get the pending events 
-GetPendingEvents: async (req,res) => {
-  try{
-    const pendingevents = await Event.find(Event.approvalStatus='pending');
-    res.json({ data: pendingevents });
-
-  }
-  catch(error) {
-   console.log(error) 
-   }
-},
-
-
-
-// Admin confirm partenr's request
-ConfirmRequest: async (req,res) => {
-   try{
-        var Eid = req.params.id
-        var findEvent = await Event.findById(Eid)
-        findEvent.approvalStatus = 'confirmed'
-        Event.updateOne({id,Eid},findEvent)
-    
-
-   }
-   catch(error) {
-    console.log(error) 
+    getEvents: async () => {
+        const events = await axios.get('http://localhost:3001/Routes/api/events/')
+        return events
+    },
+    updateEvent: async (_id) => {
+        const event = await axios.put('http://localhost:3001/Routes/api/events/5cb70f0677ccd5710821236a', {
+            eventName: 'java'
+        });
+        return event;
+    },
+    deleteEvent: async (_id) => {
+        const res = await axios.delete('http://localhost:3001/Routes/api/events/5cb717c277ccd571082123c1');
+        return res.message;
+    },
+    createEvent: async () => {
+        const newEvent = await axios.post('http://localhost:3001/Routes/api/events/', {
+          eventName: "anything",
+          organizer: "anything",
+          location: "anything",
+          description: "anything",
+          remainingPlaces: 5,
+          speakers: "anyone",
+          maximumPlaces: 7,
+          topicsCovered: "java",
+          field: "programming",
+          registrationPrice: 100,
+          approvalStatus: "approved",
+          applicants: [""],
+          feedback: [""]
+          
+        });
+        return newEvent;
     }
 }
 
-}
-router.post('/:Aid',functions.CreateEvent)
-router.post('/:Pid',functions.RequestEvent)
-router.post('/:Eid',functions.ConfirmRequest)
-router.post('/',functions.GetPendingEvents)
 module.exports = functions;
--- Start/Tests/events.test.js
+++ Start/Tests/events.test.js
@@ -1,35 +1,30 @@
-const Event = require('../../Models/Event')
-const events = require('./api/events');
-let newEvent;
-
-
-test('create a new Event', async () => {
-    const newEvent = await events.post();
-    expect(newEvent.eventName).toBe('name');
-    newEvent = newEvent;
-})
-
-
- test('get all events', async () => {
-    const vnt = await events.get();
-    expect(Array.isArray(vnt)).toBe(true);
+const funcs = require('./events.functions');
+let event;
+test('post new event', async () => {
+    const response = await funcs.createEvent()
+    expect(response.data.data.field).toEqual('programming');
 });
 
-test('get a specific Event', async (id) => {
-    const id = newEvent.id
-    const _event = await events.get(id);
-    expect(_event.id).toBe(newEvent.id);
+test('get all events', async () => {
+    const events = await funcs.getEvents();
+    expect(events).not.toBeNull();
 });
 
+// test('get specific event', async () => {
+//     const _event = await funcs.getEvents(event.id);
+//     expect(_event.id).toBe(event.id);
+// });
 
-test('update a specific Event', async (id) => {
-    const id = newEvent.id
-    const _event = await events.put(id);
-    expect(_event.eventName).toBe('updated name');
+
+test('update specific event', async () => {
+    const _event = await funcs.updateEvent();
+    console.log(_event);
+    expect(_event.data.data.eventName).toEqual('java');
 });
 
-test('delete a specific Event', async (id) => {
-    const id = newEvent.id
-    const message = await events.delete(id);
-    expect(message).toBe('Event was deleted successfully');
+test('delete specific event', async () => {
+    const message = await funcs.deleteEvent();
+    console.log(message);
+    expect(message).toEqual('Event was deleted successfully');
 });
+
