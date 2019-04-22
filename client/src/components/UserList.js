b63284e75b7e45f996661c3babfb25ac27f6bd97
KarimElMahdy <47499362+KarimElMahdy@users.noreply.github.com>
2019-04-22 12:41:50 +0200

    View Users

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
 import axios from 'axios';
 
-/*function checkUserType(userType){
-    return userType ==="consultant"
-}*/
 const textStyle = {
     textAlign: "left",
     padding: "10px",
-    fontSize: "5mm"
-  };
+    fontSize: "3.75mm"
+}
+
+
+
 export default class UserList extends React.Component {
     state = {
-        users: [ ]
+        users: [ ],
+
     }
     componentDidMount(){
        
@@ -26,21 +26,48 @@ export default class UserList extends React.Component {
 
     }
 
+    
   render() {
     const userList = this.state.users.map((User, index) =>
-     <li  style ={textStyle} key ={index}>
-     {User.name},
-     <p></p>
-     User ID: {User._id}<p></p>
-     User Type : {User.userType} <p></p>
-     Email : {User.email}  <p></p>
-     Age : {User.age} <p></p>
-     Phone Number : {User.phoneNumber}
-     </li>)
-      
+        <li  style ={textStyle} key ={index}>
+            {User.name}
+            <p></p> 
+            User Type : {User.userType}
+            <p></p> 
+            Email : {User.email}  
+            <p></p>
+            Age : {User.age}
+            <p></p> 
+            Phone Number : {User.phoneNumber}
+            <p></p>
+            Location : {User.location}
+            <p></p>
+            Skills : {User.skills}
+            <p></p>
+            Interests : {User.Interests}
+            <p></p>
+            Past Events & Tasks : {User.pastEventsAndTasks}
+            <p></p>
+            Reviews Recieved : {User.reviewsRecieved}
+            <p></p>
+            Board : {User.board}
+            <p></p>
+            Reports : {User.Reports}
+            <p></p>
+            Organisation Name : {User.organisationName}
+            <p></p>
+            Business Partners : {User.businessPartners}
+            <p></p>
+            Events Organized : {User.eventsOrganized}
+            <p></p>
+            Field of Work : {User.fieldOfWork}
+            <p></p>
+            Project History : {User.projectHistory}
+        </li>
+    )    
     return( 
          userList
-         )
+         )  
     }
 
 }
