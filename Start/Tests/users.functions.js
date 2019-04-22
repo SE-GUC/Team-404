4702dd55ea4ad0cd9198bfc8d5da4ddef4ea08a2
KarimElMahdy <47499362+KarimElMahdy@users.noreply.github.com>
2019-04-23 00:24:38 +0200

    final

--- Start/Tests/users.functions.js
+++ Start/Tests/users.functions.js
@@ -11,5 +11,17 @@ const functions = {
     const user = await axios.get("http://localhost:3001/Routes/api/users/5cb1eee8b3197064e88727bf")
     return user
   },
+  getPartners: async () => {
+    const partners = await axios.get("http://localhost:3001/Routes/api/users/partners")
+    return partners
+  },
+  getConsultants: async () => {
+    const consultants = await axios.get("http://localhost:3001/Routes/api/users/consultants")
+    return consultants
+  },
+  getCandidates: async () => {
+    const candidates = await axios.get("http://localhost:3001/Routes/api/users/candidates")
+    return candidates
+  },      
 }
 module.exports = functions;
--- Start/Tests/users.test.js
+++ Start/Tests/users.test.js
@@ -10,3 +10,18 @@ test("get specific user", async () => {
      expect(user.data.data.name).toBe("x")
     }
 )
+test('get all partners', async () => {
+    const partners = await funcs.getPartners()
+    expect(partners).not.toBeNull()
+    }
+)    
+test('get all candidates', async () => {
+    const candidates = await funcs.getCandidates()
+    expect(candidates).not.toBeNull()
+    }
+)
+test('get all consultants', async () => {
+    const consultants = await funcs.getConsultants()
+    expect(consultants).not.toBeNull()
+    }
+)
